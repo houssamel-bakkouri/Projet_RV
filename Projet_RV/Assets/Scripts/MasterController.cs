f8af3bad0a5be072e1c6c8dea1bef3588e1fa07b
Pyrano77 <pyrano77@gmail.com>
2023-03-03 12:45:19 +0100

    Yeet9

--- Projet_RV/Assets/Scripts/MasterController.cs
+++ Projet_RV/Assets/Scripts/MasterController.cs
@@ -6,36 +6,66 @@ using UnityEngine;
 public class MasterController : MonoBehaviour
 {
     public bool isHolding = false;
-    public Cube inHandCube;
-    public OVRInput.Button button2 = OVRInput.Button.Two;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<Cube> inHandCube = new List<Cube>();
+    public OVRInput.Button button;
+    public OVRInput.Controller controller;
+    
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetDown(button2))
+        if (OVRInput.Get(button) && !isHolding)
         {
-            if (isHolding) 
+            isHolding = true;
+            foreach (var cube in inHandCube)
             {
-                isHolding = false;
-                inHandCube.pickedUp = false;
+
+                cube.controller = transform;
+                cube.transform.rotation = transform.rotation;
+                if (cube.GetComponent<Rigidbody>())
+                {
+                    Rigidbody rb = cube.GetComponent<Rigidbody>();
+                    rb.isKinematic = true;
+                    rb.useGravity = false;
+                }
+            }
+        }else if (!OVRInput.Get(button) && isHolding)
+        {
+            isHolding = false;
+            foreach (var cube in inHandCube)
+            {
+
+                cube.pickedUp = false;
+                cube.controller = cube.transform;
+                if (cube.GetComponent<Rigidbody>())
+                {
+                    Rigidbody rb = cube.GetComponent<Rigidbody>();
+                    rb.isKinematic = false;
+                    rb.useGravity = true;
+                    Vector3 rawVelocity = OVRInput.GetLocalControllerVelocity(controller);
+                    Vector3 velocity = new Vector3(rawVelocity.z, rawVelocity.y, -rawVelocity.x);
+                    rb.velocity = velocity;
+
+                }
             }
         }
     }
 
-    void OnTriggerEnter(Collider other) 
+    void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent<Cube>(out var cube))
+        if (other.TryGetComponent<Cube>(out var cube) && inHandCube.Count == 0)
         {
+            inHandCube.Add(cube);
             cube.pickedUp = true;
-            cube.controller = transform;
-            inHandCube = cube;
-            isHolding = true;
-        } 
+        }
     }
 
+    void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<Cube>(out var cube) && inHandCube.Count > 0)
+        {
+            inHandCube.Remove(cube);
+        }
+    }
 }
+        
